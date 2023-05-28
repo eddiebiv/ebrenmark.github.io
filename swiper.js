e0b5222f86be8ddb772d8ecf61b7517855bd38df
eddiebiv <eddiebrenmark@gmail.com>
2023-05-27 23:38:55 -0500

    commit

---
+++ swiper.js
@@ -0,0 +1,14 @@
+let swiperProjects = new Swiper(".programs__container", {
+    loop: true,
+    spaceBetween: 24,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
